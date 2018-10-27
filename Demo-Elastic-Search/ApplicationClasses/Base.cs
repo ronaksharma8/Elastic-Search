389d320247b8028f34ea33c8ec505c32713c7b0b
Ronak <ronak.sharma@promactinfo.com>
2018-10-27 17:32:49 +0530

    added json folder

--- Demo-Elastic-Search/ApplicationClasses/Base.cs
+++ Demo-Elastic-Search/ApplicationClasses/Base.cs
@@ -7,11 +7,21 @@ namespace Demo_Elastic_Search.ApplicationClasses
     public class Base
     {
         public string Type { get; set; }
+
+        // particular entity id
         public string Additional_id { get; set; }
+        
         public string Additional_jobId { get; set; }
         public string Additional_jobTitle { get; set; }
+        public string Additional_jobCreatedDateTime { get; set; }
+        public string Additional_jobUpdateDateTime { get; set; }
+
         public string Additional_teamId { get; set; }
         public string Additional_teamIcon { get; set; }
+        public string Additional_teamCreatedDateTime { get; set; }
+        public string Additional_teamUpdateDateTime { get; set; }
+
+        // particular entity related..
         public string Additional_createdById { get; set; }
         public string Additional_createdBy { get; set; }
         public string Additional_updatedById { get; set; }
